5a8290a8fa55da510ad722ee32fd8f95dd8d2bd9
Stephen Gragg <graggsd@uab.edu>
2017-11-05 17:48:40 -0600

    Added a convert_from_NA function.

--- R/NA_vals.R
+++ R/NA_vals.R
@@ -1,23 +1,40 @@
 #' Convert values to NA
 #'
-#' \code{convert_to_NA} finds values sepcified by \code{na_vals} and sets them
+#' \code{convert_to_NA} finds values specified by \code{from} and sets them
 #' to NA.
-#' @param data A data frame
-#' @param keys One or more values that should be converted to NA.
-#' @param values 0 or more column names or positions.
-#' @seealso \code{\link[tidyr]{spread}}
+#' @param x A data frame
+#' @param from One or more values that should be converted to NA.
+#' @param cols 0 or more column names or positions.
+#' @seealso \code{\link[sgcleanup]{convert_from_NA}}
 #' @export
-convert_to_NA <- function(x, na_vals, cols = NULL) {
-
+convert_to_NA <- function(x, from, cols = NULL) {
     if (is.null(cols)) {
         cols <- colnames(x)
     }
     for (col in cols){
 
-        idx <- which(x[,col] %in% na_vals)
+        idx <- which(x[,col] %in% from)
         x[idx, col] <- NA
     }
-
     return(x)
+}
 
+#' Convert values from NA to an alternative value
+#'
+#' \code{convert_from_NA} finds NA values and changes them to a value specified
+#' by \code{to}
+#' @param x A data frame
+#' @param to Values that NA values should be converted to
+#' @param cols 0 or more column names or positions.
+#' @seealso \code{\link[sgcleanup]{convert_to_NA}}
+#' @export
+convert_from_NA <- function(x, to, cols = NULL) {
+    if (is.null(cols)) {
+        cols <- colnames(x)
+    }
+    for (col in cols){
+        idx <- which(is.na(x[,col]))
+        x[idx, col] <- to
+    }
+    return(x)
 }
