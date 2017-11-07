6d3919ee79204498c097c74383a354069df1a794
Stephen Gragg <graggsd@uab.edu>
2017-11-07 14:30:53 -0600

    Updated the format_p_val function to include the ability to convert values below a cutoff to scientific notation.

--- R/format_p_val.R
+++ R/format_p_val.R
@@ -1,20 +1,35 @@
 #' Format p-value with a specified significant digit cutoff
 #'
-#' \code{format_p_value} will take a set of p-values, round to the specified
+#' Takes a set of p-values, rounds to the specified
 #' significant digits, and convert values that are less than the specified
-#' cutoff to "<'cutoff'"
+#' cutoff to "<'cutoff'". Maintains consistent numbers of significant digits.
 #'
 #' @param x A vector of p-values
 #' @param digits how many significant digits are to be used.
+#' @param cutoff_action Determines the behavior of the function for values that
+#' are less than the specified number of digits. If set to \code{"inequality"},
+#' then the function replaces these values with \code{"<cutoff"}. If set to
+#' \code{"sci"}, then the values are converted to scientific notation.
 #' @return A vector of formatted p-values.
 #' @export
-format_p_val <- function(x, digits = 3) {
+format_p_val <- function(x, digits = 3, cutoff_action = "inequality") {
     if (is.character(x)) {
         x <- as.numeric(x)
     }
     cutoff <- 10^(-digits)
-    idx <- which(x < cutoff)
-    x <- as.character(round(x, digits))
-    x[idx] <- paste0("<", cutoff)
+
+    idx1 <- which(x < cutoff)
+    if (cutoff_action == "inequality") {
+        less <- paste0("<", cutoff)
+    } else if(cutoff_action == "sci") {
+        less <- format(x[idx1], digits = digits)
+    }
+
+    idx2 <- which(x >= cutoff)
+    greater <- format(round(x[idx2], digits = digits), nsmall = digits)
+
+    x[idx1] <- less
+    x[idx2] <- greater
+
     return(x)
 }
