7814f36531191114da0d9b18c3db38905f77010c
Stephen Gragg <graggsd@uab.edu>
2017-11-08 21:37:18 -0600

    Actually...this time I removed the get_first_class function

--- R/log_reg_models.R
+++ R/log_reg_models.R
@@ -1,16 +1,4 @@
 
-#' get_first_class <- function(x, ...) {
-#'     UseMethod("get_first_class", x)
-#' }
-#'
-#' get_first_class.list <- function(x, ...) {
-#'     return(sapply(x, function(x) {class(x)[1]}))
-#' }
-#'
-#' get_first_class.default <- function(x, ...) {
-#'     return(class(x)[1])
-#' }
-#'
 
 #' Adds an odds Ratio and 95% confidence interval
 #'
