a13b7bc581193136b10593ba418a15bbeb10c64b
Stephen Gragg <graggsd@uab.edu>
2017-11-08 21:36:11 -0600

    Removed the get_first_class function and added a function to add ORs and Upper and Lower bounds to a data.frame

--- R/log_reg_models.R
+++ R/log_reg_models.R
@@ -1,12 +1,29 @@
+
+#' get_first_class <- function(x, ...) {
+#'     UseMethod("get_first_class", x)
+#' }
+#'
+#' get_first_class.list <- function(x, ...) {
+#'     return(sapply(x, function(x) {class(x)[1]}))
+#' }
+#'
+#' get_first_class.default <- function(x, ...) {
+#'     return(class(x)[1])
+#' }
+#'
+
+#' Adds an odds Ratio and 95% confidence interval
+#'
 #' @export
-get_first_class <- function(x, ...) {
-    UseMethod("get_first_class", x)
+add_OR_bounds <- function(x, ...) {
+    UseMethod("add_OR_bounds", x)
 }
 
-get_first_class.list <- function(x, ...) {
-    return(sapply(x, function(x) {class(x)[1]}))
-}
-
-get_first_class.default <- function(x, ...) {
-    return(class(x)[1])
+add_OR_bounds.data.frame <- function(x, ...) {
+    stopifnot("estimate" %in% colnames(x),
+              "std.error" %in% colnames(x))
+    x$OR <- exp(x$estimate)
+    x$Lower <- exp(x$estimate - 1.96*x$std.error)
+    x$Upper <- exp(x$estimate + 1.96*x$std.error)
+    return(x)
 }
