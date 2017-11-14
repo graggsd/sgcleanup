dad5e97f4c8af613876a23b04dc693c40bb0d43e
Stephen Gragg <graggsd@uab.edu>
2017-11-13 19:00:57 -0600

    Added a tabulate_wd function

---
+++ R/tabulation.R
@@ -0,0 +1,19 @@
+#' Tabulate the columns of wide data
+#'
+#' Counts unique values of specified columns and returns these counts as a
+#' data.frame
+#' @param dat A data.frame
+#' @param cols Columns to tabulate
+#' @return A data.frame with original \code{cols} as rows and columns containing
+#' the counts of individual values
+#' @export
+tabulate_wd <- function(dat, cols) {
+    dat %>%
+        select(cols) %>%
+        mutate(idx = 1:nrow(data)) %>%
+        gather_("column", "value", cols) %>%
+        group_by(column, value) %>%
+        summarise(count = n()) %>%
+        spread(value, count, fill = 0) %>%
+        as.data.frame(stringsAsFactors = FALSE)
+}
