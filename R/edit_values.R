847ea3074cc383d767eeb009226c55f49bfc1b64
Stephen Gragg <graggsd@uab.edu>
2018-05-25 13:36:09 -0500

    Added more error checking in edit_values to make sure that row_key is contained in both data and ref

--- R/edit_values.R
+++ R/edit_values.R
@@ -1,6 +1,13 @@
 #' @export
-edit_values <- function(data, ref, row_key, column_key,
+edit_values <- function(data,
+                        ref,
+                        row_key,
+                        column_key,
                         value){
+
+    # Check that row_key is contained in both data and ref
+    stopifnot(row_key %in% colnames(data),
+              row_key %in% colnames(ref))
 
     # Check for consistency of rows in reclass dictionary
     if (length(setdiff(ref[, row_key], data[, row_key])) > 0) {
@@ -15,6 +22,11 @@ edit_values <- function(data, ref, row_key, column_key,
              setdiff(setdiff(cols, colnames(data))))
     }
 
+    # For each row in the reference dictionary, find the value in the
+    # row_key column and the value in the column_key column
+    # Use the value in the column key to select the appropriate column in
+    # data and (assuming the row_key column name is the same for both
+    # the reference and the dataset are the same - verified above)
     for (i in 1:nrow(ref)) {
         row <- ref[i, row_key]
         col <- ref[i, column_key]
